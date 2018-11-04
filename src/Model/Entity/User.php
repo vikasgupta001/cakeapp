cb8765669821da09d64cf5580019281d2cf0fac5
vikashgupta <cse.vikashgupta@gmail.com>
2018-11-04 11:54:06 +0530

    Added Password Hashing feature to Site

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Model\Entity;
-
+use Cake\Auth\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 /**
@@ -41,4 +41,10 @@ class User extends Entity
     protected $_hidden = [
         'password'
     ];
+    protected function _setPassword($password)
+    {
+        if (strlen($password) > 0) {
+          return (new DefaultPasswordHasher)->hash($password);
+        }
+    }
 }
